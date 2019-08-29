d8ad17cda85128af2cd20a906ea77356d62e5d26
sam-masaki <sam.masaki@protonmail.com>
2019-08-28 20:42:40 -0700

    Added custom.el that I forgot to add earlier

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (speed-type slime flycheck-cstyle org git-gutter flycheck esup projectile ag dumb-jump hungry-delete whitespace-cleanup-mode yasnippet-snippets aggressive-indent pdf-tools yaml-mode use-package solarized-theme sml-mode smartparens rainbow-mode rainbow-delimiters racket-mode python org-bullets moe-theme lua-mode latex-preview-pane langtool gruvbox-theme google-c-style flyspell-popup elpy company-math company-auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
