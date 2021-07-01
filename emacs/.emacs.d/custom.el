26c84f4177d9631cc34d50aaa968e382c7603af2
Sam Lewis <sam.masaki@pm.me>
2021-06-30 22:10:10 -0700

    aaa

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (speed-type slime flycheck-cstyle org git-gutter flycheck esup projectile ag dumb-jump hungry-delete whitespace-cleanup-mode yasnippet-snippets aggressive-indent pdf-tools yaml-mode use-package solarized-theme sml-mode smartparens rainbow-mode rainbow-delimiters racket-mode python org-bullets moe-theme lua-mode latex-preview-pane langtool gruvbox-theme google-c-style flyspell-popup elpy company-math company-auctex))))
+   '(toml-mode company-c-headers company google-c-style yaml-mode moe-theme dumb-jump ag slime projectile git-gutter whitespace-cleanup-mode hungry-delete smartparens use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
